e702eeebd0a47e8814c1d6229791a7024e2b715b
IvanKutin <kutinivan2003@mail.ru>
2022-07-04 13:41:38 +0300

    task-2: Шаблон Базы Данных

--- skillcheck/testenglish/models.py
+++ skillcheck/testenglish/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=255)
+    parent = models.CharField(max_length=255)
+
+
+class Test(models.Model):
+    author = models.CharField(max_length=255)
+    title = models.CharField(max_length=255)
+    type_test = models.CharField(max_length=255)
+    desc = models.TextField(blank=True)
+    complexity = models.CharField(max_length=255)
+    category = Category()
+    created_date = models.CharField(max_length=255)
+
+
+class Question(models.Model):
+    test = Test()
+    text = models.TextField(blank=True)
+    answer1 = models.CharField(max_length=255)
+    answer2 = models.CharField(max_length=255)
+    answer3 = models.CharField(max_length=255)
+    answer4 = models.CharField(max_length=255)
+    correct_answer = models.CharField(max_length=255)
+
+
+class User(models.Model):
+    user = models.CharField(max_length=255)
+    test = Test()
+    result = models.CharField(max_length=255)
