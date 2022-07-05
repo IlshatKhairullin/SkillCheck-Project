3c7acbc9c34fce1b265f30abbff0c7750384f88a
IvanKutin <kutinivan2003@mail.ru>
2022-07-05 12:41:49 +0300

    task-2:Добавлена База Данных

--- skillcheck/testenglish/models.py
+++ skillcheck/testenglish/models.py
@@ -1,32 +1,27 @@
 from django.db import models
 
 
-class Category(models.Model):
-    title = models.CharField(max_length=255)
-    parent = models.CharField(max_length=255)
-
 
 class Test(models.Model):
-    author = models.CharField(max_length=255)
     title = models.CharField(max_length=255)
     type_test = models.CharField(max_length=255)
     desc = models.TextField(blank=True)
     complexity = models.CharField(max_length=255)
-    category = Category()
     created_date = models.CharField(max_length=255)
 
 
 class Question(models.Model):
-    test = Test()
+    test = models.ForeignKey(Test, on_delete=models.PROTECT, null=True)
     text = models.TextField(blank=True)
     answer1 = models.CharField(max_length=255)
     answer2 = models.CharField(max_length=255)
     answer3 = models.CharField(max_length=255)
     answer4 = models.CharField(max_length=255)
     correct_answer = models.CharField(max_length=255)
+    category = models.CharField(max_length=255, null=True)
 
 
 class User(models.Model):
     user = models.CharField(max_length=255)
-    test = Test()
+    test = models.ForeignKey(Test, on_delete=models.PROTECT, null=True)
     result = models.CharField(max_length=255)
